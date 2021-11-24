78b07dd55cd79dd64a8cd3fee0433ccd4374b1dc
geniusserg <danilovsergeydmitrievich@gmail.com>
2021-11-24 12:23:12 +0300

    users/id/games/id playtime post works

--- model/user.ts
+++ model/user.ts
@@ -1,7 +1,6 @@
 import { ObjectId } from "mongodb";
 import Game from "./game";
 export default class User {
-    constructor(public name: string, 
-        public hours: number,
+    constructor(public username: string, 
         public games: {game: string, playTime: number}[], public id?: ObjectId) {}
 }
--- routers/users.ts
+++ routers/users.ts
@@ -34,6 +34,14 @@ usersRouter.get("/:id", async (req: Request, res: Response) => {
 usersRouter.post("/", async (req: Request, res: Response) => {
     try {
         const newUser = req.body as User;
+        if (!newUser.games){
+            newUser.games = [];
+        }
+        for (let i = 0; i < newUser.games.length; i++){
+            if (!newUser.games[i].playTime){
+                newUser.games[i].playTime = 0;
+            }
+        }
         const result = await collections.users?.insertOne(newUser);
         result
             ? res.status(201).send({"id": result.insertedId})
@@ -85,9 +93,17 @@ usersRouter.get("/:id/games", async (req: Request, res: Response) => {
     try {
         const query = { _id: new ObjectId(id) };
         const user = (await collections.users?.findOne(query)) as unknown as User;
-
+        var games: Game[] = [];
+        var records: {game: Game, playTime: number}[] = [];
         if (user) {
-            res.status(200).send(user.games);
+            for (let i = 0; i < user.games.length; i++){
+                const game = (await collections.games?.findOne({ _id: new ObjectId(user.games[i].game) })) as unknown as Game;
+                games.push(game);
+            }
+            for (let i = 0; i < user.games.length; i++){
+                records.push({game: games[i], playTime: user.games[i].playTime ?? 0});
+            }
+            res.status(200).send({ games: records });
         }
     } catch (error) {
         res.status(404).send(`Unable to find matching document with id: ${req.params.id}`);
@@ -99,9 +115,9 @@ usersRouter.post("/:id/games", async (req: Request, res: Response) => {
     try {
         const gameId = req.body.id;
         const query = { _id: new ObjectId(userId) };
-        const game = (await collections.games?.findOne({ _id: new ObjectId(gameId) })) as unknown as User;
+        const game = (await collections.games?.findOne({ _id: new ObjectId(gameId) })) as unknown as Game;
         if (game) {
-            const result = await collections.users?.updateOne(query, { $push: { "games" : gameId}});
+            const result = await collections.users?.updateOne(query, { $push: { "games": { "game" : gameId, "playTime": 0 }}});
             result
             ? res.status(201).send({})
             : res.status(500).send("Failed to create a new user.");
@@ -120,18 +136,13 @@ usersRouter.post("/:id/games/:gid", async (req: Request, res: Response) => {
     try {
         const gameId = req?.params?.gid;
         var playtime = req.body.playTime;
+        console.info(`lets update ${gameId} playtime for ${userId} with vlaue ${playtime}`)
         const query = { _id: new ObjectId(userId) }
         const user = (await collections.users?.findOne(query)) as unknown as User;
-        for ( let i = 0; i < user.games.length; i++) {
-          if (user.games[i].game == gameId){
-            let r = `games.${i}.playTime`
-            const result = await collections.users?.updateOne(query, { $set: { r : playtime}});
-            result
-            ? res.status(201).send({})
-            : res.status(500).send("Failed to update play time.");
-          }  
-        };
-        res.status(500).send("Failed to update play time.");
+        const result = await collections.users?.updateOne({_id: new ObjectId(userId), 'games': { $elemMatch: { game: gameId}}}, {$set: { "games.$.playTime" : playtime}});
+        result
+        ? res.status(201).send({})
+        : res.status(500).send("Failed to update play time.");
     } catch (error) {
         console.error(error);
         res.status(400).send((error as Error).message);
